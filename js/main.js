fde0d4f82b86cde000d0b1b11faf4b42492576ec
Егор <egor@MBP-Egor.netis.cc>
2024-12-03 13:29:09 +0100

    fix bags

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const arrow = document.querySelector('.hero__doubledown');
 console.log(arrow);
 function handleClick(){
-    window.scrollBy(0, document.documentElement.clientHeight)
+    window.scrollBy({top: document.documentElement.clientHeight, left: 0, behavior: "smooth"})
 }
-
+// 0, document.documentElement.clientHeight
 
 arrow.addEventListener('click', handleClick)
